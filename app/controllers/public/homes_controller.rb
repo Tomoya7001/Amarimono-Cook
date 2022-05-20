881e89c87b5b30ae30c969fa0b7d78ff9db0cdf2
Tomoya7001 <spmiwdar7001@gmail.com>
2022-05-20 10:38:43 +0000

    [fix]swiper

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+    # 新着順４まで
     @cooks = Cook.order(created_at: :desc).limit(4)
-    #いいねランキング上位４
+    # いいねランキング上位４
     @rank = Cook.find(Favorite.group(:cook_id).order("count(cook_id) desc").limit(4).pluck(:cook_id))
     #@cook = Cook.find(params[:id])
   end
